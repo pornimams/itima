f52eb0fa2ebe941689a62b83022101e448a0f7ba
Pornimams <porinma5730@gmail.com>
2022-12-08 16:59:26 +0530

    first commit

---
+++ python.py
@@ -0,0 +1,7 @@
+P = input("\n Enter the principal amount: ")
+T = input("\n Enter the time: ")
+R = input("\n Enter the rate: ")
+Si = (int(P) * float(T) * float(R) ) /100
+Ci = int(P) * (((1 + float(R)/100) ** int(T)) - 1)
+print("\n Simple Interest = ",Si)
+print("\n Compound Interest = ",Ci)
